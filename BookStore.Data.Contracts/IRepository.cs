9f3f54725d9948fdac2d3df21e10d01468d04e8b
dnd <duong.nguyen91@gmail.com>
2016-08-24 17:47:01 +0700

    Init project

---
+++ BookStore.Data.Contracts/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace BookStore.Data.Contracts
+{
+    public interface IRepository<TEntity>
+            where TEntity : class
+    {
+        TEntity GetByID(object id);
+
+        List<TEntity> GetAll();
+
+        TEntity Add(TEntity entity);
+
+        TEntity Update(TEntity entity);
+
+        bool Delete(object id);
+
+        int Count(Expression<Func<TEntity, bool>> filter);
+
+        TEntity Get(Expression<Func<TEntity, bool>> filter);
+
+        IEnumerable<TEntity> GetList(Expression<Func<TEntity, bool>> filter);
+
+    }
+}
